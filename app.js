17c331cb8137c42ba3dbee782a2a02020ab0dd76
Eric Lima <eric.george.lima@gmail.com>
2016-02-17 00:42:04 -0200

    correcoes

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ webApp = angular.module('WebApp', ['ngRoute', 'ui.bootstrap']);
 webApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
 		when('/about', {
-			templateUrl: '/views/about.html',
+			templateUrl: 'views/about.html',
 			controller: function(){}
 		}).
 		otherwise({
-			templateUrl: '/views/homepage.html',
+			templateUrl: 'views/homepage.html',
 			controller: 'HomepageController'
 		});
 }]);
